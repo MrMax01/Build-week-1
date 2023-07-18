8d65c6a7dec8f40b8e5dea2021dc63d6dae6f66f
MrMax01 <massimo.mauro00@gmail.com>
2023-07-18 13:13:39 +0200

    new start

---
+++ benchmark.js
@@ -0,0 +1,8 @@
+/*
+1. Selezionare il main con queryselector('main') e caricare in modo casuale la domanda con le possibili risposte.
+2.al click di uno delle risposte il bottone si dovrà colorare di viola. Se una risposta è già stata selezionata e ne clicchiamo un altra, la risposta selezionata precedentemente perderà il colore e verrà colorato quella nuova . 
+3.Nel momento in ci l'utente clicca prosegui prendi la risposta data e controlli se è corretta. Se è corretta oppure no verrà salvata in un array dove verrà visualizzato il punteggio dell'utente (insieme di risposte esatte e errate ---> [true, flase, true, true, false, true])
+
+4. una volta risposto l'ultima domanda il tasto PROSEGUI ci indirizzerà nella pagina 'result page'
+*/
+console.log(questions[0].question);
