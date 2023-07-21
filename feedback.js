80316fdf8f291016d5474496da0b19f8f4a3b5b5
MrMax01 <massimo.mauro00@gmail.com>
2023-07-21 12:00:40 +0200

    feedback done

--- feedback.js
+++ feedback.js
@@ -14,3 +14,26 @@ stars.forEach((star) => {
   });
   //   star.classList.add("selected");
 });
+
+const body = document.querySelector("body");
+
+body.addEventListener("keyup", (event) => {
+  // console.log(event.key);
+  if (event.key === "Enter") {
+    document.querySelector("input").innerText = "";
+    const p = document.getElementById("msg");
+    p.innerHTML = "";
+    const numberStar = document.querySelectorAll(".star.selected ~ .star");
+    console.log(numberStar);
+    if (numberStar.length + 1 <= 6) {
+      p.innerHTML =
+        "Siamo spiacenti che non siamo stati all'altezza delle tue aspettative. Siamo aperti a suggerimenti per migliorarci. &#x1F614;";
+    } else if (numberStar.length + 1 > 6 && numberStar.length < 8) {
+      p.innerHTML = "Grazie! Dicci dove possiamo migliorare per ottenere il voto pieno! &#128516;";
+    } else {
+      p.innerHTML = "Siamo contenti che i servizi da noi offerti siano stati soddisfacenti! &#x1F498;";
+    }
+
+    document.querySelector("footer").appendChild(p);
+  }
+});
